b5925937ded44c8bb99b926671f5aa3b279a13f4
Warren Smyth <warrensmyth@me.com>
2021-01-07 16:08:16 -0600

    Move navigation bar from layout to navigation partial

--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -13,6 +13,7 @@ class PalindromeAppTest < Minitest::Test
   	assert doc(last_response).at_css('h1')
     assert_equal "Learn Enough Ruby Sample App | Home",
                   doc(last_response).at_css('title').content
+    assert doc(last_response).at_css('nav')
   end
 
   def test_about
