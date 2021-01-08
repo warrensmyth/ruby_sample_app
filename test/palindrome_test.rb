5f9307daf4ff6d1a6a7e34ccc1a1c85f9b437614
Warren Smyth <warrensmyth@me.com>
2021-01-08 13:33:45 -0600

    Refactor palindrome form & add to results page

--- test/palindrome_test.rb
+++ test/palindrome_test.rb
@@ -12,7 +12,12 @@ class PalindromeAppTest < Minitest::Test
     assert doc(last_response).at_css('form')
   end
 
-  
+  def test_non_palindrome_submission
+    post '/check', phrase: "Not a palindrome"
+    assert_includes doc(last_response).at_css('p').content, "isn't a palindrome"
+    assert doc(last_response).at_css('form')
+  end
+
   def test_palindrome_submission
     post '/check', phrase: "Able was I, ere I saw Elba."
     assert_includes doc(last_response).at_css('p').content, "is a palindrome"
